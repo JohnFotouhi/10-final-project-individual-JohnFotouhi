92918e6d30a77a0febdf99ed59e73f00b0b5ebf5
JohnFotouhi <fotouman>
2021-12-12 15:06:54 -0600

    Updates

---
+++ precache-manifest.1f906d2b6635288920577517777f072b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64a07086200426726b38",
+    "url": "/10-final-project-individual-JohnFotouhi/static/css/main.6c800747.chunk.css"
+  },
+  {
+    "revision": "64a07086200426726b38",
+    "url": "/10-final-project-individual-JohnFotouhi/static/js/main.f2aafa7c.chunk.js"
+  },
+  {
+    "revision": "107b0ebfaa2717ec2cc9",
+    "url": "/10-final-project-individual-JohnFotouhi/static/js/runtime~main.68b23e55.js"
+  },
+  {
+    "revision": "c755c432be641f66065e",
+    "url": "/10-final-project-individual-JohnFotouhi/static/css/2.5287a0c2.chunk.css"
+  },
+  {
+    "revision": "c755c432be641f66065e",
+    "url": "/10-final-project-individual-JohnFotouhi/static/js/2.50844e10.chunk.js"
+  },
+  {
+    "revision": "85d12f8c1162f5015c378a7301fc8a06",
+    "url": "/10-final-project-individual-JohnFotouhi/index.html"
+  }
+];
