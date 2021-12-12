5d27c4260d2972a2a15efe05fb3eeaa2247ff71e
JohnFotouhi <fotouman>
2021-12-12 14:51:35 -0600

    beta

---
+++ final-project/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Education from './Education.jsx';
+
+function App() {
+  return (
+    <Education/>
+  );
+}
+
+export default App;
